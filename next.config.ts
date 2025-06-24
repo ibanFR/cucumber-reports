fe3beaf1123a3f9791237c498d8206802471d71e
David Goss <david@davidgoss.co>
2025-06-24 10:46:19 +0100

    Enable source maps (#10)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: true
 }
 
 export default createMDX({})(nextConfig)
