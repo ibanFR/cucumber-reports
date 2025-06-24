4061e024d6524c8cef1feef7ff3ec5d9de7ac777
David Goss <david@davidgoss.co>
2025-06-24 11:17:08 +0100

    Add Sentry for observability (#11)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from 'next'
 import createMDX from '@next/mdx'
+import { withSentryConfig } from '@sentry/nextjs'
 
 const nextConfig: NextConfig = {
-  productionBrowserSourceMaps: true
+  productionBrowserSourceMaps: true,
 }
 
-export default createMDX({})(nextConfig)
+export default withSentryConfig(
+  createMDX()(nextConfig),
+  {
+    org: 'cucumber-i5',
+    project: 'cucumber-reports',
+    silent: !process.env.CI,
+    widenClientFileUpload: true,
+    disableLogger: true,
+  },
+)
